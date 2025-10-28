24f2c13a9e44acaadd40982f4f7e1a98b3a4ef99
Xurxo <152525151+Xurxo8@users.noreply.github.com>
2025-10-28 16:32:20 +0100

    Actualización uninstall.php
    Mejora de la seguridad

--- ho_lista_enlaces/sql/uninstall.php
+++ ho_lista_enlaces/sql/uninstall.php
@@ -29,6 +29,10 @@
  * Maybe the merchant will just try to reset the module
  * but does not want to loose all of the data associated to the module.
  */
+if (!defined('_PS_VERSION_')) {
+  exit;
+}
+
 $sql = array();
 
 foreach ($sql as $query) {
